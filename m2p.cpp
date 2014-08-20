e621862d6def5853e28821e830b2c82ff6bae6b0
steniu01 <niuyunfei2010@hotmail.com>
2014-08-21 00:31:29 +0100

    Update m2p.cpp

--- m2p.cpp
+++ m2p.cpp
@@ -8,7 +8,6 @@ volatile uint32_t DMAErrCount = 0;
 void DMA_IRQHandler (void);
 uint32_t DMA_Init( uint32_t DMAMode );
 
-Serial pc (USBTX, USBRX);
 
  
 int main(void)
@@ -25,11 +24,9 @@ int main(void)
     uint32_t DMA_DST = (uint32_t)dst;
 
     DMA_InitTypeDef dma_init_struct;
-	/*
     DMA_StructInit(&dma_init_struct);
 
- 
-	
+    /* Initialize structure */
     dma_init_struct.DMA_DestAddr = DMA_DST;
     dma_init_struct.DMA_SrcAddr = DMA_SRC;
     dma_init_struct.DMA_TransferSize = size;
@@ -41,7 +38,8 @@ int main(void)
     dma_init_struct.DMA_DestInc = 1;
     dma_init_struct.DMA_TermInt = 1; // enable count interrupt:w
     dma_init_struct.DMA_TransferType = M2M;
-    LPC_SC->PCONP |= (1 << 29);    // Enable GPDMA clock 
+    
+    LPC_SC->PCONP |= (1 << 29);    /* Enable GPDMA clock */
     DMA_init(LPC_GPDMACH0, &dma_init_struct); //initialize the channel
     LPC_GPDMA->DMACConfig = 0x01; // enable DMA and little endian
     while ( (LPC_GPDMA->DMACConfig & 0x01) == 0);
@@ -56,7 +54,8 @@ int main(void)
 
     
     printf("src text: %s", src);
-    while (!DMATCCount); 
+
+    while (!DMATCCount);  /* Wait untypedil DMA is done */
 
 
     printf("dst text: %s", dst);
@@ -64,22 +63,18 @@ int main(void)
     if (strcmp (src, dst) != 0)
         printf("error!");
     else
-        printf("correct!\r\n");
-*/
+        printf("correct!");
+
 
 /*test m2P, send the memory data to UART;*/
     /* Initialize structure */
-    pc.printf ("start to test M2P");
-		DMA_StructInit(&dma_init_struct);
+    DMA_StructInit(&dma_init_struct);
     DMATCCount = 0;
     DMAErrCount = 0;
 
-    LPC_UART0->FCR |= 1;
+    dma_init_struct.DMA_DestAddr = DMA_DST;
     LPC_UART0->FCR |= 1<<3;
-		LPC_UART0->FCR &= ~(0x11<<6); 
-		
-		dma_init_struct.DMA_DestAddr = (uint32_t)LPC_UART0->THR;
-    //dma_init_struct.DMA_DestAddr = (uint32_t)LPC_UART0->THR;
+    dma_init_struct.DMA_DestAddr = (uint32_t)LPC_UART0->THR;
     dma_init_struct.DMA_SrcAddr = DMA_SRC; 
     dma_init_struct.DMA_TransferSize = size;
     dma_init_struct.DMA_SrcBurst = 0x01; // 1 byte
@@ -89,24 +84,24 @@ int main(void)
     dma_init_struct.DMA_SrcInc = 1;
     dma_init_struct.DMA_DestInc = 0;
     dma_init_struct.DMA_TermInt = 1; // enable count interrupt:w
-		dma_init_struct.DMA_TransferType = M2P;
-		dma_init_struct.DMA_DestPeripheral = 0x1000;
- 
+
+
     DMA_init(LPC_GPDMACH1, &dma_init_struct); //initialize the channel
-		LPC_SC->PCONP |= (1 << 29);    // Enable GPDMA clock 
     LPC_GPDMA->DMACConfig = 0x01; // enable DMA and little endian
     while ( (LPC_GPDMA->DMACConfig & 0x01) == 0);
 
-pc.printf ("to test M2P");
+    // set the IRQ routine
+    NVIC_SetVector(DMA_IRQn, (uint32_t)DMA_IRQHandler);
+    NVIC_EnableIRQ(DMA_IRQn);
+
     // Have to be in this order, I don't know why
     DMA_Cmd(LPC_GPDMACH1, ENABLE); // enable channel
     DMA_ITConfig (LPC_GPDMACH1, DMA_ITC, ENABLE);
-    NVIC_SetVector(DMA_IRQn, (uint32_t)DMA_IRQHandler);
-    NVIC_EnableIRQ(DMA_IRQn);
-    
-    //printf("new src text: %s", src);
 
-    while (!DMATCCount);  /* Wait until DMA is done */
+    
+    printf("new src text: %s", src);
+
+    while (!DMATCCount);  /* Wait untypedil DMA is done */
 
 
 
@@ -116,7 +111,6 @@ pc.printf ("to test M2P");
 void DMA_IRQHandler (void)
 {
     uint32_t regVal;
-	DMATCCount++;
     regVal = LPC_GPDMA->DMACIntTCStat;
     if ( regVal )
     {
