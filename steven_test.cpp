009c073098bbc07ff8e74ac4679491a42172a5d7
steniu01 <niuyunfei2010@hotmail.com>
2014-08-21 12:03:50 +0100

    Update steven_test.cpp

--- steven_test.cpp
+++ steven_test.cpp
@@ -1,50 +1,130 @@
 #include "mbed.h"
-#include "LPC1768_dma.h"
-DigitalOut myled(LED1);
 
-int main() {
-    uint32_t busyChannels;
-    printf("Start\r\n");
+#include "LPC1768_dma.h"
+
+volatile uint32_t DMATCCount = 0;
+volatile uint32_t DMAErrCount = 0;
+
+void DMA_IRQHandler (void);
+uint32_t DMA_Init( uint32_t DMAMode );
+
+RawSerial pc (USBTX, USBRX);
+
+  
+int main(void)
+{
     
-    printf("Use DMA to send 10 characters from buffer to buffer\r\n");
-    printf("Then send them to UART0 (PC)\r\n");
+    /* test the DMA M2M, copy data from src to dest, and then print out the dest mem data */
+    printf("start to test DMA M2M test!\r\n");
     
-    char characters[] = "abcdefgh\r\n";     //Characters we send
-    char characters2[11];                   //Second buffer
-   // LPC_SC->PCONP |= 1<<29;//power on
-    DMA_InitTypeDef* new_dma;
-    DMA_StructInit (new_dma);
+    char src[] = "Hello world! \r\n";
+    uint8_t size = sizeof (src);
+    char *dst  = (char *) malloc(size);
+    memset (dst, '\0', size);
+
+    uint32_t DMA_SRC = (uint32_t)src;
+    uint32_t DMA_DST = (uint32_t)dst;
+
+    DMA_InitTypeDef dma_init_struct;
+    DMA_StructInit(&dma_init_struct);
+
+    /* Initialize structure */
+    dma_init_struct.DMA_DestAddr = DMA_DST;
+    dma_init_struct.DMA_SrcAddr = DMA_SRC;
+    dma_init_struct.DMA_TransferSize = size;
+    dma_init_struct.DMA_SrcBurst = 0x01; // 1 byte
+    dma_init_struct.DMA_DestBurst = 0x01;
+    dma_init_struct.DMA_SrcWidth = 0x00; // byte
+    dma_init_struct.DMA_DestWidth = 0x00;
+    dma_init_struct.DMA_SrcInc = 1;
+    dma_init_struct.DMA_DestInc = 1;
+    dma_init_struct.DMA_TermInt = 1; // enable count interrupt:w
+    dma_init_struct.DMA_TransferType = M2M;
     
-    new_dma->DMA_DestAddr= (uint32_t)characters2; /*!< Specifies the destination base address for DMAy Channelx. */
-    new_dma->DMA_SrcAddr= (uint32_t)characters;     /*!< Specifies the source base address for DMAy Channelx. */
-    new_dma->DMA_TransferSize=10;/*!< Specifies the source  transfer size    */
-  //  new_dma->DMA_SrcBurst; /*!< Specifies the source  burst size    */
-  //  new_dma->DMA_DestBurst; /*!< Specifies the destination burst size   */
-  //  new_dma->DMA_SrcWidth; /*!< Specifies the source transfer width   */
-  //  new_dma->DMA_DestWidth; /*!< Specifies the destination transfer width   */
-  //  new_dma->DMA_SrcInc;  /*!< Specifies whether the source is incremented or not */
-  //  new_dma->DMA_DestInc; /*!< Specifies whether the destination is incremented or not */
-    new_dma->DMA_TermInt = 1; /*!< Specifies whether the terminal count interrupt enabled or not */
+    LPC_SC->PCONP |= (1 << 29);    /* Enable GPDMA clock */
+    DMA_init(LPC_GPDMACH0, &dma_init_struct); //initialize the channel
+    LPC_GPDMA->DMACConfig = 0x01; // enable DMA and little endian
+    while ( (LPC_GPDMA->DMACConfig & 0x01) == 0);
+
+    // set the IRQ routine
+    NVIC_SetVector(DMA_IRQn, (uint32_t)DMA_IRQHandler);
+    NVIC_EnableIRQ(DMA_IRQn);
+
     
-    /*!< Specifies the features set by channel config register */
-    new_dma->DMA_SrcPeripheral=0;
-    new_dma->DMA_DestPeripheral=0;
-    new_dma->DMA_TransferType=0;
+    DMA_Cmd(LPC_GPDMACH0, ENABLE); // enable channel
+    DMA_ITConfig (LPC_GPDMACH0, DMA_ITC, ENABLE);
+
     
+    printf("src text: %s", src);
+
+    while (!DMATCCount);  /* Wait until DMA is done */
+
+
+    printf("dst text: %s", dst);
+
+    if (strcmp (src, dst) != 0)
+        printf("error! \r\n");
+    else
+        printf("correct! \r\n");
+
     
-    busyChannels= DMA_EnabledChannels(); 
-    if (!(busyChannels && 0x1))
-    {
-        DMA_init(LPC_GPDMACH0, new_dma);
-        DMA_ClearITPendingBit(LPC_GPDMACH0,DMA_ITC);
-        DMA_ClearITPendingBit(LPC_GPDMACH0,DMA_IE);
-        DMA_ITConfig (LPC_GPDMACH0,DMA_ITC,DISABLE);
-        DMA_ITConfig (LPC_GPDMACH0, DMA_IE,DISABLE);
-        printf("I am going to start");
-        DMA_Cmd(LPC_GPDMACH0, ENABLE);
+
+/*test m2P, send the memory data to UART;*/
+    char src2[] = "Hello world! This message was transmitted via UART DMA from memory \r\n";
+    uint8_t size2 = sizeof (src2);
+
+    uint32_t DMA_SRC2 = (uint32_t)src2;
+    pc.printf ("start to test M2P \r\n");
+
+ /* Initialize structure */
+    LPC_UART0->FCR |= 1<<3 ; //enable UART DMA mode
+
+    dma_init_struct.DMA_DestAddr = (uint32_t) &(LPC_UART0->THR);
+    dma_init_struct.DMA_SrcAddr = DMA_SRC2; 
+    dma_init_struct.DMA_TransferSize = size2;
+    dma_init_struct.DMA_SrcBurst = 0x00; // 1 byte
+    dma_init_struct.DMA_DestBurst = 0x00;
+    dma_init_struct.DMA_SrcWidth = 0x00; // byte
+    dma_init_struct.DMA_DestWidth = 0x00;
+    dma_init_struct.DMA_SrcInc = 1;
+    dma_init_struct.DMA_DestInc = 0;
+    dma_init_struct.DMA_TermInt = 1; // enable count interrupt:w
+    dma_init_struct.DMA_TransferType = M2P;
+    dma_init_struct.DMA_DestPeripheral = 8;
+ 
+    LPC_SC->PCONP |= (1 << 29);    // Enable GPDMA clock 
         
-        //while (DMA_ChannelActive(LPC_GPDMACH0));
+    DMA_init(LPC_GPDMACH1, &dma_init_struct); //initialize the channel
+    LPC_GPDMA->DMACConfig = 0x01; // enable DMA and little endian; have to initial firstly then enable
+    while ( (LPC_GPDMA->DMACConfig & 0x01) == 0);
+
+        
+    NVIC_SetVector(DMA_IRQn, (uint32_t)DMA_IRQHandler);
+    NVIC_EnableIRQ(DMA_IRQn);
+    DMA_Cmd(LPC_GPDMACH1, ENABLE); // enable channel
+    DMA_ITConfig (LPC_GPDMACH1, DMA_ITC, ENABLE);
+    
+
+    while ( DMA_EnabledChannels() && 2);
+
+    while (1);
+}
+
+void DMA_IRQHandler (void)
+{
+    uint32_t regVal;
+    DMATCCount++;
+    regVal = LPC_GPDMA->DMACIntTCStat;
+    if ( regVal )
+    {
+        DMATCCount++;
+        LPC_GPDMA->DMACIntTCClear |= regVal;
     }
-    wait(10);
-    printf ("the second buffer now is %s", characters2 );
+    regVal = LPC_GPDMA->DMACIntErrStat;
+    if ( regVal )
+    {
+        DMAErrCount++;
+        LPC_GPDMA->DMACIntErrClr |= regVal;
+    }
+    return;
 }
