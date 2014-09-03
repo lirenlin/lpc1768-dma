8eec364c8b1466a7b15848292e90f1ec6f96858b
steniu01 <niuyunfei2010@hotmail.com>
2014-09-03 18:43:15 +0100

    Update dma.h

--- dma.h
+++ dma.h
@@ -1,126 +1,101 @@
-#include "LPC1768_dma.h"
+
+#include "dma_api.h"
 #include "mbed.h"
 
-
-enum TriggerType;
-
-class DMA
-{
-public:
-    DMA_InitTypeDef dma_init_struct;
-    LPC_GPDMACH_TypeDef* GPDMA_Channel;
-    DMA(int prority); // constructor
-    DMA ();
-    void source (uint32_t src, int width, bool inc);
-    void destination (uint32_t dst, int width, bool inc);
-    void getTransferType (TransferType type);
-    void start ( int length);
-    bool wait();
-    void trigger(TriggerType trig);
-
-    void attach_TC(void (*fptr)(void)) {
-        _TCCallback.attach(fptr);
-        NVIC_SetVector(DMA_IRQn, (uint32_t)&IRQ_handler);
-        NVIC_EnableIRQ(DMA_IRQn);
-    }
-    template<typename T> //has to use template in the header file
-    void attach_TC(T* tptr, void (T::*mptr)(void)) {
-        if((mptr != NULL) && (tptr != NULL)) {
-            _TCCallback.attach(tptr, mptr);
-        }
-        NVIC_SetVector(DMA_IRQn, (uint32_t)&IRQ_handler);
-        NVIC_EnableIRQ(DMA_IRQn);
-    }
-
-    void attach_Err(void (*fptr)(void)) {
-        _ErrCallback.attach(fptr);
-        NVIC_SetVector(DMA_IRQn, (uint32_t)&IRQ_handler);
-        NVIC_EnableIRQ(DMA_IRQn);
-    }
-    template<typename T> //has to use template in the header file
-    void attach_Err(T* tptr, void (T::*mptr)(void)) {
-        if((mptr != NULL) && (tptr != NULL)) {
-            _ErrCallback.attach(tptr, mptr);
-        }
-        NVIC_SetVector(DMA_IRQn, (uint32_t)&IRQ_handler);
-        NVIC_EnableIRQ(DMA_IRQn);
-    }
-
-
-private:
-    FunctionPointer _ErrCallback; // if declare as a function pointer, it will hang when attach
-    FunctionPointer _TCCallback;
-    int channel;
-    static DMA* dma;
-    TransferType type;
-    int chooseFreeChannel(int channel);
-    static void IRQ_handler(void); // use static because this method will be pased as a pointer, it has to be static. un-static method in Class will only have address in runtime.
-
-
-};
-
-
-#ifdef TARGET_LPC1768
-
-enum TriggerType {
-    ALWAYS = -1,
-    SSP0_TX,
-    SSP0_RX,
-    SSP1_TX,
-    SSP1_RX,
-    ADC,
-    I2S0,
-    I2S1,
-    DAC,
-    UART0_TX,
-    UART0_RX,
-    UART1_TX,
-    UART1_RX,
-    UART2_TX,
-    UART2_RX,
-    UART3_TX,
-    UART3_RX,
-    MATCH0_0 = 24,
-    MATCH0_1,
-    MATCH1_0,
-    MATCH1_1,
-    MATCH2_0,
-    MATCH2_1,
-    MATCH3_0,
-    MATCH3_1
-};
-
-#endif
-
-//extern "C" void IRQ_handler(void);
-
-
-
-/*
-template <typename T>
-void DMA::init (T src, T dst, TransferType type)
-{
-    dma_init_struct.DMA_DestAddr = (uint32_t)dst;
-    dma_init_struct.DMA_SrcAddr = (uint32_t)src;
-    //dma_init_struct.DMA_TransferSize = size;
-    dma_init_struct.DMA_SrcBurst = 0x01; // 1 byte
-    dma_init_struct.DMA_DestBurst = 0x01;
-    dma_init_struct.DMA_SrcWidth = 0x00;//sizeof(*src); // caculate the width according to the input
-    dma_init_struct.DMA_DestWidth = 0x00;//sizeof(*dst);
-		{
-		if (type==M2M || type==M2P)
-				dma_init_struct.DMA_SrcInc = 1;
-		else
-			  dma_init_struct.DMA_SrcInc = 0;
-		}
-		{
-		if (type==M2M || type==P2M)
-r			 dma_init_struct.DMA_DestInc = 1;
-		else
-			 dma_init_struct.DMA_DestInc = 0;
-		}
-
-
-    dma_init_struct.DMA_TransferType = type;
-}
+/** A generic DMA for transfer data without hanging the CPU.
+ * It can be used for m2m, m2p, p2m, m2m transfer
+ * It will choose the DMA with the priority you have set. 
+ * Lower number means higher prority. 
+ * If no prority is set, it will choose which ever free channel.
+ * Example:
+ * @code
+ * // Send the memory data "Hello world" in source address to destination address via DMA
+ * // attach a function to swtich LED on when the transfer finish 
+ *	char src[] = "Hello world\r\n";
+ *	uint8_t size = sizeof (src);
+ *  char *dst  = (char *) malloc(size);
+ *  memset (dst, '\0', size);
+ *  LPC_SC->PCONP |= (1 << 29);    // Enable LPC1768 GPDMA clock 
+ *  DMA dma1 (0) ;
+ *  dma1.source (src,0,1);
+ *	dma1.destination (dst,0,1);
+ *	dma1.attach_TC(led_switchon) ;
+ *  dma1.start(size);
+ *  dma1.wait();
+ *  printf("dst text: %s", dst);
+ * @endcode
 */
+
+
+
+class DMA {
+public:
+		/** @brief  Create the DMA Channel according to the prority or choose whichever free if no prority given. 
+		*					  Initialize the number of channels in the device
+		*  @param   priority: The DMA prority.
+		*	 @note 	  If prority has been given but that channel is not avaiable, it will wait until the channel avaiable.
+		*/
+		DMA(int priority = -1); 
+	
+		/** @brief  Get source starting address, transfer width and setting auto increment.
+		 *  @param  src. The source starting address. 
+		 *	@param  width. The transfer data width
+		 *  @param  inc. Set memory automatice increment.
+		 *	@note 	If width is not given, it will set the width automatically according to the data type in the memory	
+		 *					if increment is not given. It will set automatically according to whether the source is within memory range.
+		 */
+		template<typename T>    // To be reviewd, should put the template into the cpp rather than the header?
+    void source (T* src, int width = (sizeof(T)-1), bool inc = 0){
+				inc = isMemory ((uint32_t) src); // To be reviewed. How to put this line into the arguments?
+		    DMA_source(&dma_init_struct, (uint32_t)src, width, inc);
+		}
+		template<typename T> 
+    void destination (T* dst, int width = (sizeof(T)-1), bool inc = 0){
+			inc = isMemory ((uint32_t) dst);
+			DMA_destination(&dma_init_struct, (uint32_t)dst, width, inc);
+		}
+		void trigger(TriggerType trig);
+    void start ( int length);  
+    void wait();
+
+		void attach_TC(void (*fptr)(void)){
+                _TCCallback.attach(fptr);
+                NVIC_SetVector(DMA_IRQn, (uint32_t)&IRQ_handler);
+                NVIC_EnableIRQ(DMA_IRQn);   
+        }
+    template<typename T> //has to use template in the header file
+        void attach_TC(T* tptr, void (T::*mptr)(void)) {  
+                if((mptr != NULL) && (tptr != NULL)) {
+                _TCCallback.attach(tptr, mptr);
+                }
+                NVIC_SetVector(DMA_IRQn, (uint32_t)&IRQ_handler);
+                NVIC_EnableIRQ(DMA_IRQn); 
+        }
+
+    void attach_Err(void (*fptr)(void)){
+            _ErrCallback.attach(fptr);
+            NVIC_SetVector(DMA_IRQn, (uint32_t)&IRQ_handler);
+            NVIC_EnableIRQ(DMA_IRQn);   
+        }
+        template<typename T> 
+        void attach_Err(T* tptr, void (T::*mptr)(void)) {  
+                if((mptr != NULL) && (tptr != NULL)) {
+                        _ErrCallback.attach(tptr, mptr);
+                }
+                NVIC_SetVector(DMA_IRQn, (uint32_t)&IRQ_handler);
+                NVIC_EnableIRQ(DMA_IRQn); 
+        }
+    DMA_InitTypeDef dma_init_struct;
+		int channel_num;
+    
+protected: 
+      FunctionPointer _ErrCallback ; // if declare as a function pointer, it will hang when attach
+      FunctionPointer _TCCallback;   	
+      int chan;
+      static DMA* dma ;// To be reviewed, whether should declare a instance here?
+      int chooseFreeChannel(int channel);
+      static void IRQ_handler( void); // need to be static to be passed to the NVIC_SetVector
+
+};
+
+
